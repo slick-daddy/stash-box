982574398d735944971fded8f38dbf529a147b40
Slick Daddy <129640104+slick-daddy@users.noreply.github.com>
2026-08-20 23:10:49 +0300

    Optimize slice comparison utility

--- pkg/utils/slice_compare.go
+++ pkg/utils/slice_compare.go
@@ -11,17 +11,43 @@ func Includes[T comparable](arr []T, against T) bool {
 }
 
 func SliceCompare[T comparable](subject []T, against []T) (added []T, missing []T) {
+	againstSet := makeSet(against)
+	subjectSet := makeSet(subject)
+	addedSet := make(map[T]struct{}, len(subject))
+	missingSet := make(map[T]struct{}, len(against))
+
 	for _, v := range subject {
-		if !Includes(against, v) && !Includes(added, v) {
-			added = append(added, v)
+		if _, found := againstSet[v]; found {
+			continue
 		}
+		if _, found := addedSet[v]; found {
+			continue
+		}
+
+		added = append(added, v)
+		addedSet[v] = struct{}{}
 	}
 
 	for _, v := range against {
-		if !Includes(subject, v) && !Includes(missing, v) {
-			missing = append(missing, v)
+		if _, found := subjectSet[v]; found {
+			continue
 		}
+		if _, found := missingSet[v]; found {
+			continue
+		}
+
+		missing = append(missing, v)
+		missingSet[v] = struct{}{}
 	}
 
 	return
+}
+
+func makeSet[T comparable](values []T) map[T]struct{} {
+	ret := make(map[T]struct{}, len(values))
+	for _, v := range values {
+		ret[v] = struct{}{}
+	}
+
+	return ret
 }
